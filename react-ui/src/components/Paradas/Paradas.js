438d20e91d0b382bba5e0f0acec9cdfc846872b2
thor25 <trainero2001@yahoo.es>
2020-12-31 19:16:34 +0100

    rokes

--- react-ui/src/components/Paradas/Paradas.js
+++ react-ui/src/components/Paradas/Paradas.js
@@ -1,7 +1,35 @@
-import Rect from "react"
-
-
+import Rect, {useState,useEffect} from "react"
+import firebase, { firestore } from "../../firebase"
 
 export default function Paradas () {
-    return 'Paradas'
+
+  const [paradas, setparadas] = useState([])
+  const [linea, setlinea] = useState('01')
+  useEffect(() => {
+
+    const paradasRef =   firestore.collection("paradas")
+    let parada = paradasRef.where("LINEA", "==", "01").get()
+    .then(snapshot => {
+      if (snapshot.empty) {
+        console.log('No matching documents.');
+        return;
+      }
+  
+      snapshot.forEach(doc => {
+        console.log(doc.id, '=>', doc.data());
+      });
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+  
+
+      console.log('datos')
+  }, [])
+  
+    return (
+      <>
+      Hola
+      </>
+    )
 }
