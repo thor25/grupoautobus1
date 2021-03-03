72a5b5d61611dde099e286bb97001f3ccc500b50
thor25 <trainero2001@yahoo.es>
2021-03-03 09:12:44 +0100

    actualizado server

--- server/index.js
+++ server/index.js
@@ -408,7 +408,7 @@ const getItem =  (key) => {
   {
     console.log("users-post")
     var id = req.body.params
-    if (isDev===false)
+    if (isDev===true)
     {
       var valor=JSON.stringify(tiempo)
      
