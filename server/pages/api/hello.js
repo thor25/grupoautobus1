2a79410078d8efc7b51f7a43b301825790095825
thor25 <40572988+thor25@users.noreply.github.com>
2021-07-04 19:14:49 +0200

    eliminado server

--- server/pages/api/hello.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { firebaseInit } from "../../db/firebase"
-import {signIn} from "../../db/firestore.init"
-export default async function handler(req, res) {
-  await firebaseInit();
-  var grupos = await signIn(process.env.DB_USERNAME,process.env.DB_PASSWORD)
-  console.log("🚀 ~ file: hello.js ~ line 7 ~ handler ~ grupos", grupos)
-  res.status(200).json({ datos: grupos })
-}
--- server/pages/api/times.js
+++
@@ -1,7 +0,0 @@
-export default async function handler(req, res) {
-    await firebaseInit();
-  
-    res.status(200).json({ datos: grupos })
-  }
-
- 
